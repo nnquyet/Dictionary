afdb3c4fa55f4c9e621fd9af743908e3c4f002fb
nnquyet <starmoondz1811@gmail.com>
2022-04-21 22:46:26 +0700

    Dictionary

---
+++ source_code/Dictionary/src/Main.java
@@ -0,0 +1,11 @@
+package src;
+
+public class Main {
+    public static void main(String[] args) {
+        DictionaryCommandline commandline = new DictionaryCommandline();
+        commandline.dictionaryAdvance();
+        DictionaryApplication application = new DictionaryApplication();
+        application.runApplication();
+    }
+}
+
